bcc4286e0f7715ea02eb5150327bdcfda5410e0a
MilenaPL <92209272+MilenaPL@users.noreply.github.com>
2021-11-15 13:39:06 +0100

    math and strings created

---
+++ math_strings.js
@@ -0,0 +1,55 @@
+Math.floor(3.233)
+Math.random();
+Math.random () *100;
+Math.floor(Math.random ()*100)+1;
+
+let score=150;
+score
+let division=2;
+let tt= score/division
+tt
+
+88%5
+50%7
+let content="coding for fun"
+content
+
+
+let games = "Heroes 3"
+games[3]
+games+" "+content
+"hi"+5
+"hi"-"u"
+games.toUpperCase()
+games
+games=games.toUpperCase()
+games
+games.slice(4)
+games.slice(2,3)
+games.slice(0,6)
+games.slice(7)
+games.slice(6)
+
+'game over'.replace('g','aaa')
+'I am naughty'.replace('naughty','like an angel');
+
+const money = 6456+999;
+money
+const add= "444"+"66"
+add
+'Who you looking for?'.slice(1).replace("for","or");
+
+`my mom said\"be happy\"`
+
+"Hello\nWorld"
+
+
+
+
+
+
+
+
+
+
+
